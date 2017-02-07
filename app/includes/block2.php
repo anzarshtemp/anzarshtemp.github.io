6b542ac98fff4adee880ba54b5a8cf90bd2aaaf3
iwebnal <iwebnal@gmail.com>
2017-02-07 14:17:50 +0300

    a

--- app/includes/block2.php
+++ app/includes/block2.php
@@ -6,7 +6,7 @@
     	<div class="row">
 			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
 				<div class="ah-aboutus-imgbox">
-					<img src="img/b2_girl.png" alt="girl">
+					<img src="img/b2_girl.png" class="img-responsive" alt="girl">
 				</div>
 			</div>
 			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
