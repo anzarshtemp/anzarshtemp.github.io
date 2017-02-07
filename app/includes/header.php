74105bdeeb2f3ae5f74f1f379224f66f6750b563
anzarshtemp <anzarshtemp@mail.ru>
2017-02-07 13:42:55 +0300

    fonts

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,14 +1,34 @@
-<header class="header">
+<header class="az-header">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-4">
-				<img src="img/logo.png" alt="logo">
+				<a href="/"><img src="img/logo.png" alt="logo"></a>
 			</div>
 			<div class="col-md-4">
-				хедер
+				<div class="az-header-middle">
+					<div class="az-select az-right az-select_style az-margin-bottom10">
+						<select>
+							<option value="1">Санкт-Петербург</option>
+							<option value="2">Москва</option>
+							<option value="3">Красноярск</option>
+						</select>
+					</div>
+					<span class="az-city">г. Москва, ул. Московская, 123А</span>
+				</div>
 			</div>
 			<div class="col-md-4">
-				хедер
+				<div class="az-info">
+					<div class="az-info-left">
+						<a href="tel:88001002030" class="az-phone">8 800 100 20 30</a>
+						<a href="mailto:info@5zkt.ru" class="az-email">info@5zkt.ru</a>
+					</div>
+					<div class="az-info-soc">
+						<a href="#" class="az-vk"></a>
+						<a href="#" class="az-facebook"></a>
+						<a href="#" class="az-ok"></a>
+						<a href="#" class="az-instagram"></a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
