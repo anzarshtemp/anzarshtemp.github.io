18542c7e2cdf8a414eed08104181d59b7d1b97ae
iwebnal <iwebnal@gmail.com>
2017-02-07 21:55:19 +0300

    block3

--- app/includes/block3.php
+++ app/includes/block3.php
@@ -3,10 +3,14 @@
 <section class="ah-maincat">
     <div class="container-fluid">
     	<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-				<div class="ah-maincat-wrapp ah-maincat-left">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h2 class="ah-aboutus-title">Основные категории</h2>
+	    	</div>
+	    	<div class="clearfix"></div>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<div class="ah-maincat-wrapp ah-maincat-left ah-maincat-height">
 					<div class="ah-maincat-imgbox">
-						<img src="img/b3_1.png" alt="composite rebar">
+						<img src="img/b3_1.png" style="width: 43px; height: 117px" alt="composite rebar">
 					</div>
 					<div class="ah-maincat-textbox">
 						<span class="ah-maincat-span">Композитная арматура</span>
@@ -14,10 +18,10 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-				<div class="ah-maincat-wrapp ah-maincat-right">
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<div class="ah-maincat-wrapp ah-maincat-right ah-maincat-height">
 					<div class="ah-maincat-imgbox">
-						<img src="img/b3_2.png" alt="composite mesh">
+						<img src="img/b3_2.png" style="width: 98px; height: 75px" alt="composite mesh">
 					</div>
 					<div class="ah-maincat-textbox">
 						<span class="ah-maincat-span">Композитная сетка</span>
@@ -28,36 +32,36 @@
 
 			<div class="clearfix"></div>
 
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-				<div class="ah-maincat-wrapp">
-					<div class="ah-maincat-imgbox">
-						<img src="img/b3_3.png" alt="flexible communications">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				<div class="ah-maincat-wrapp ah-maincat-lit1">
+					<div class="ah-maincat-imgbox1">
+						<img src="img/b3_3.png" class="img-responsive" alt="flexible communications">
 					</div>
-					<div class="ah-maincat-textbox">
-						<span class="ah-maincat-span">Композитная сетка</span>
-						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-				<div class="ah-maincat-wrapp">
-					<div class="ah-maincat-imgbox">
-						<img src="img/b3_4.png" alt="Composite pipes">
-					</div>
-					<div class="ah-maincat-textbox">
-						<span class="ah-maincat-span">Композитная сетка</span>
-						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					<div class="ah-maincat-textbox1">
+						<span class="ah-maincat-span">Гибкие связи</span>
+						<p class="ah-maincat-taxt">Краткое описание категории.</p>
 					</div>
 				</div>
 			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-				<div class="ah-maincat-wrapp">
-					<div class="ah-maincat-imgbox">
-						<img src="img/b3_5.png" alt="findings">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				<div class="ah-maincat-wrapp ah-maincat-lit2">
+					<div class="ah-maincat-imgbox1">
+						<img src="img/b3_4.png" style="width: 97px; height: 80px" alt="Composite pipes">
 					</div>
-					<div class="ah-maincat-textbox">
-						<span class="ah-maincat-span">Композитная сетка</span>
-						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					<div class="ah-maincat-textbox1">
+						<span class="ah-maincat-span">Композитные трубы</span>
+						<p class="ah-maincat-taxt">Краткое описание категории.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				<div class="ah-maincat-wrapp ah-maincat-lit3">
+					<div class="ah-maincat-imgbox1">
+						<img src="img/b3_5.png" style="width: 75px; height: 100px" alt="findings">
+					</div>
+					<div class="ah-maincat-textbox1">
+						<span class="ah-maincat-span">Фурнитура</span>
+						<p class="ah-maincat-taxt">Краткое описание категории.</p>
 					</div>
 				</div>
 			</div>
