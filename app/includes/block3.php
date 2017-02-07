c20c9755c671156d1ad98dc16f978277d7516014
iwebnal <iwebnal@gmail.com>
2017-02-07 14:42:51 +0300

    s

---
+++ app/includes/block3.php
@@ -0,0 +1,68 @@
+<div class="clearfix"></div>
+
+<section class="ah-maincat">
+    <div class="container-fluid">
+    	<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+				<div class="ah-maincat-wrapp ah-maincat-left">
+					<div class="ah-maincat-imgbox">
+						<img src="img/b3_1.png" alt="composite rebar">
+					</div>
+					<div class="ah-maincat-textbox">
+						<span class="ah-maincat-span">Композитная арматура</span>
+						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+				<div class="ah-maincat-wrapp ah-maincat-right">
+					<div class="ah-maincat-imgbox">
+						<img src="img/b3_2.png" alt="composite mesh">
+					</div>
+					<div class="ah-maincat-textbox">
+						<span class="ah-maincat-span">Композитная сетка</span>
+						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<div class="ah-maincat-wrapp">
+					<div class="ah-maincat-imgbox">
+						<img src="img/b3_3.png" alt="flexible communications">
+					</div>
+					<div class="ah-maincat-textbox">
+						<span class="ah-maincat-span">Композитная сетка</span>
+						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<div class="ah-maincat-wrapp">
+					<div class="ah-maincat-imgbox">
+						<img src="img/b3_4.png" alt="Composite pipes">
+					</div>
+					<div class="ah-maincat-textbox">
+						<span class="ah-maincat-span">Композитная сетка</span>
+						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<div class="ah-maincat-wrapp">
+					<div class="ah-maincat-imgbox">
+						<img src="img/b3_5.png" alt="findings">
+					</div>
+					<div class="ah-maincat-textbox">
+						<span class="ah-maincat-span">Композитная сетка</span>
+						<p class="ah-maincat-taxt">Описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории, описание категории.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="clearfix"></div>
