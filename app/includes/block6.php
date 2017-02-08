f62a4a9fc4740a9d819249dd380371e65c3febbf
iwebnal <iwebnal@gmail.com>
2017-02-08 15:34:28 +0300

    table

---
+++ app/includes/block6.php
@@ -0,0 +1,69 @@
+<div class="clearfix"></div>
+
+<section class="ah-comparative-box">
+    <div class="container-fluid">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h2 class="ah-aboutus-title">Сравнительная таблица</h2>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-comparative-table">
+					<table class="ah-table ah-table-border">
+						<thead>
+							<tr>
+								<th class="ah-table-border1 ah-table-padd ah-table-back"><span>Свойства</span></th>
+								<th class="ah-table-padd"><span>Композитная арматура</span></th>
+								<th class="ah-table-padd"><span>Обычная арматура</span></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Прочность</td>
+								<td class="ah-table-item"><span class="ah-tabitem1">Прочность</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem11">Прочность</span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Экономичность</td>
+								<td class="ah-table-item"><span class="ah-tabitem2">Экономичность</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem22">Экономичность</span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Электропроводность</td>
+								<td class="ah-table-item"><span class="ah-tabitem3">Электропроводность</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem33">Электропроводность</span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Срок службы</td>
+								<td class="ah-table-item"><span class="ah-tabitem4">Срок службы</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem44">Срок службы</span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Вес</td>
+								<td class="ah-table-item"><span class="ah-tabitem5">Вес</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem55">Вес</span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Устойчивость к коррозии</td>
+								<td class="ah-table-item"><span class="ah-tabitem6">Устойчивость к коррозии</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem66"><span class="ah-tabitem66-span">Устойчивость к коррозии</span></span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Длина</td>
+								<td class="ah-table-item"><span class="ah-tabitem7">Длина</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem77">Длина</span></td>
+							</tr>
+							<tr>
+								<td class="ah-table-border1 ah-table-item1">Устойчивость к температурам</td>
+								<td class="ah-table-item"><span class="ah-tabitem8">Устойчивость к температурам</span></td>
+								<td class="ah-table-item"><span class="ah-tabitem88">Устойчивость к температурам</span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="clearfix"></div>
