6bd7f133a546c3b6d19aa6c756a6595041b21399
iwebnal <iwebnal@gmail.com>
2017-02-08 12:38:00 +0300

    a

--- app/includes/block5.php
+++ app/includes/block5.php
@@ -3,46 +3,119 @@
 <section class="ah-advantages" id="tabs">
     <div class="container-fluid">
     	<div class="row">
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 no-padd">
+			<div class="col-lg-4 col-md-4 col-sm-5 col-xs-12 no-padd">
 				<div class="ah-advantages-wrapp">
-					<span class="ah-advantages-title">Преимущества</span>
 					<ul class="tabs-navigation">
-						<li><a href="#tab-1">В 3 раза прочнее</a></li>
-						<li><a href="#tab-2">30% экономия</a></li>
-						<li><a href="#tab-3">Диэлектрик</a></li>
-						<li><a href="#tab-4">80 лет службы</a></li>
-						<li><a href="#tab-5">В 10 раз легче</a></li>
-						<li><a href="#tab-6">Не ржавеет</a></li>
-						<li><a href="#tab-7">Любая длина</a></li>
-						<li><a href="#tab-8">Устойчивость к температурам</a></li>
+						<li>
+							<a href="#tab-0" class="ah-advantages-title ah-tab1_link1">
+							Преимущества</a>
+						</li>
+						<li>
+							<a href="#tab-1" class="ah-tab1_link1">
+							<img src="img/tab11.png" style="width: 50px; height: 50px;" alt="tab1">В 3 раза прочнее</a>
+
+						</li>
+						<li>
+							<a href="#tab-2" class="ah-tab1_link2">
+							<img src="img/tab12.png" style="width: 50px; height: 50px;" alt="tab1">30% экономия</a>
+
+						</li>
+						<li>
+							<a href="#tab-3" class="ah-tab1_link3">
+							<img src="img/tab13.png" style="width: 50px; height: 50px;" alt="tab1">Диэлектрик</a>
+
+						</li>
+						<li>
+							<a href="#tab-4" class="ah-tab1_link4">
+							<img src="img/tab14.png" style="width: 50px; height: 50px;" alt="tab1">80 лет службы</a>
+
+						</li>
+						<li>
+							<a href="#tab-5" class="ah-tab1_link5">
+							<img src="img/tab15.png" style="width: 50px; height: 50px;" alt="tab1">В 10 раз легче</a>
+
+						</li>
+						<li>
+							<a href="#tab-6" class="ah-tab1_link6">
+							<img src="img/tab16.png" style="width: 50px; height: 50px;" alt="tab1">Не ржавеет</a>
+
+						</li>
+						<li>
+							<a href="#tab-7" class="ah-tab1_link7">
+							<img src="img/tab17.png" style="width: 50px; height: 50px;" alt="tab1">Любая длина</a>
+
+						</li>
+						<li>
+							<a href="#tab-8" class="ah-tab1_link8">
+							<img src="img/tab18.png" style="width: 50px; height: 50px;" alt="tab1">Устойчивость к температурам</a>
+
+						</li>
 					</ul>
 				</div>
 			</div>
-			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 no-padd">
+			<div class="col-lg-8 col-md-8 col-sm-7 col-xs-12 no-padd">
 				<div class="tabs-container">
+					<div id="tab-0">
+						<span class="ah-content_tab1-title">Композитная арматура</span>
+						<div class="ah-content_tab1-contbox">
+							<img src="img/arm.png" class="img-responsive" alt="arm">
+						</div>
+			        </div>
 			        <div id="tab-1">
-			            Контент 1 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">В 3 раза прочнее</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-2">
-			            Контент 2 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">30% экономия</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-3">
-			            Контент 3 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">Диэлектрик</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-4">
-			            Контент 4 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">80 лет службы</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-5">
-			            Контент 5 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">В 10 раз легче</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-6">
-			            Контент 6 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">Не ржавеет</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-7">
-			            Контент 7 таба
+						<span class="ah-content_tab1-title">Композитная арматура</span>
+						<div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">Любая длина</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			        <div id="tab-8">
-			            Контент 8 таба
+			            <span class="ah-content_tab1-title">Композитная арматура</span>
+			            <div class="ah-content_tab1-cont">
+			            	<span class="ah-content_tab1-span">Устойчивость к температурам</span>
+			            	<p class="ah-content_tab1-text">Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества, Описание преимущества,</p>
+			            </div>
 			        </div>
 			    </div>
 			</div>
