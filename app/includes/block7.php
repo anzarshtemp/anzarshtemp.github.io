ae48f33b5c1d71ec3da8c04a1abc6078f252c2b6
anzarshtemp <anzarshtemp@mail.ru>
2017-02-08 17:48:45 +0300

    tabs

---
+++ app/includes/block7.php
@@ -0,0 +1,44 @@
+<section class="az-tabs">
+	<div class="az-tab-content">
+		<div class="az-tab-content1">
+			<div class="az-tab-inner" style="background: url(img/az-tab1.jpg) no-repeat top center; background-size: cover">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-12">
+							<h2 class="az-h2-style2">Применение</h2>
+							<hr>
+							<span class="az-tab-subtitle">Коттеджное строительство</span>
+							<p>Описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела, описание раздела.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="az-tab-switcher">
+		<div class="az-tabswitch1">
+			<span class="az-switch-title">Малоэтажное строительство</span>
+			<span class="az-switch-subtitle">Малоэтажное строительство</span>
+		</div>
+		<div class="az-tabswitch2">
+			<span class="az-switch-title">Малоэтажное строительство</span>
+			<span class="az-switch-subtitle">Малоэтажное строительство</span>
+		</div>
+		<div class="az-tabswitch3">
+			<span class="az-switch-title">Малоэтажное строительство</span>
+			<span class="az-switch-subtitle">Малоэтажное строительство</span>
+		</div>
+		<div class="az-tabswitch4">
+			<span class="az-switch-title">Малоэтажное строительство</span>
+			<span class="az-switch-subtitle">Малоэтажное строительство</span>
+		</div>
+		<div class="az-tabswitch5">
+			<span class="az-switch-title">Малоэтажное строительство</span>
+			<span class="az-switch-subtitle">Малоэтажное строительство</span>
+		</div>
+		<div class="az-tabswitch6">
+			<span class="az-switch-title">Малоэтажное строительство</span>
+			<span class="az-switch-subtitle">Малоэтажное строительство</span>
+		</div>
+	</div>
+</section>
